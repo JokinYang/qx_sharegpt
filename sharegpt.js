a2fcf5800240ffaaf5c921b8e7e40b57275cdf3e
Jokin Yang <yangjun97@foxmail.com>
2023-05-25 18:55:44 +0800

    Update sharegpt.js

--- sharegpt.js
+++ sharegpt.js
@@ -1,6 +1,6 @@
 let regex_body = '</body>';
 // let js = '<script async="async" src="https://cdn.jsdelivr.net/gh/JokinYang/qx_sharegpt/sharegpt_logic.js"></script></body>';
-let js = '<script async="async" src="https://cdn.jsdelivr.net/gh/domeccleston/sharegpt/sharegpt-extension/index.js"></script></body>';
+let js = '<script defer src="https://cdn.jsdelivr.net/gh/domeccleston/sharegpt/sharegpt-extension/index.js"></script></body>';
 let body = $response.body.replace(regex_body, js);
 $done({
     body
